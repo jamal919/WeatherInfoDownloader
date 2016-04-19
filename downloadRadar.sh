9c457d33f507a1bff268e769c6a1c59c02fb80fd
Jamal Uddin Khan <jamal919@gmail.com>
2016-04-19 13:28:42 +0600

    FIX: Folder Fix

--- downloadRadar.sh
+++ downloadRadar.sh
@@ -16,21 +16,22 @@ while true; do
 	wget -c http://bmd.gov.bd/file/img/nwp/radar_molvibazar.gif
 
 	# Creating Directory
-	mkdir `date +"%Y%m%d%H%M%S"`
+	folderName=$(date +"%Y%m%d_%H%M%S")
+	mkdir $folderName
 	# Renaming with datetime
-	mv ppi_agt.gif ./`date +"%Y%m%d%H%M%S"`/ppi_agt.gif
-	mv ppz_agt.gif ./`date +"%Y%m%d%H%M%S"`/ppz_agt.gif
-	mv ppv_agt.gif ./`date +"%Y%m%d%H%M%S"`/ppv_agt.gif
-	mv caz_agt.gif ./`date +"%Y%m%d%H%M%S"`/caz_agt.gif
-	mv sri_agt.gif ./`date +"%Y%m%d%H%M%S"`/sri_agt.gif
-	mv pac_agt.gif ./`date +"%Y%m%d%H%M%S"`/pac_agt.gif
-	mv vp2_agt.gif ./`date +"%Y%m%d%H%M%S"`/vp2_agt.gif
+	mv ppi_agt.gif ./$folderName/.
+	mv ppz_agt.gif ./$folderName/.
+	mv ppv_agt.gif ./$folderName/.
+	mv caz_agt.gif ./$folderName/.
+	mv sri_agt.gif ./$folderName/.
+	mv pac_agt.gif ./$folderName/.
+	mv vp2_agt.gif ./$folderName/.
 	
 	# Renaming
-	mv radar_dhaka.gif ./`date +"%Y%m%d%H%M%S"`/radar_dhaka.gif
-	mv radar_cox.gif ./`date +"%Y%m%d%H%M%S"`/radar_cox.gif
-	mv radar_khepupara.gif ./`date +"%Y%m%d%H%M%S"`/radar_khepupara.gif
-	mv radar_molvibazar.gif ./`date +"%Y%m%d%H%M%S"`/radar_molvibazar.gif
+	mv radar_dhaka.gif ./$folderName/.
+	mv radar_cox.gif ./$folderName/.
+	mv radar_khepupara.gif ./$folderName/.
+	mv radar_molvibazar.gif ./$folderName/.
 	
 	sleep 3600
 
